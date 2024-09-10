589ae48f7a19a50e9ca3db08dcf9bca5ec882512
Dylan Shafe <143671996+Knightlife27@users.noreply.github.com>
2024-09-10 20:38:25 +0000

    update Dotenv

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
@@ -8,8 +9,9 @@ module.exports = merge(common, {
     },
     plugins: [
         new Dotenv({
-            safe: true,
-            systemvars: true
+            systemvars: true, // This will load all system environment variables
+            safe: false, // Set this to false to avoid the need for a .env.example file
+            defaults: false // Don't load a .env file by default
         })
     ]
 });
