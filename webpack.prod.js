361f5a9c38c10fa3d90c2906718b2ce085d31747
Dylan Shafe <143671996+Knightlife27@users.noreply.github.com>
2024-09-12 00:40:38 +0000

    update

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
@@ -5,6 +6,8 @@ const webpack = require('webpack');
 module.exports = merge(common, {
     mode: 'production',
     output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
         publicPath: '/'
     },
     module: {
