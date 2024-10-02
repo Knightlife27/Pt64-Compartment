83578c8ad8199470f9868fba6c999375454607e2
Jeff Shafe <jshafe@heartmath.org>
2024-10-02 16:46:17 -0700

    Replace code spaces URL with a Heroku URL

--- src/front/js/component/homeSearchPage.js
+++ src/front/js/component/homeSearchPage.js
@@ -33,7 +33,7 @@ export const HomeSearchPage = () => {
     //   console.log("Backend URL:", process.env.REACT_APP_BACKEND_URL);
 
     //   const response = await fetch (`https://verbose-space-acorn-7vjq4wxrxwfpv5w-3001.app.github.dev/api/analyze_apartments`, {
-      const response = await fetch (`AIzaSyBnfyPlPR8dahULrOxvBT7UDjKlAGOkqNE`, {
+      const response = await fetch (`https://backend-nestify-5c7b2f6794c9.herokuapp.com/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ preferences })
