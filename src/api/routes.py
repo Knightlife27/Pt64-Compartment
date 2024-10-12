69d811f95c0d1ee6f339a43053a77ed07406aa42
Jeff Shafe <jshafe@heartmath.org>
2024-10-12 14:30:48 -0700

    Troubleshooting

--- src/api/routes.py
+++ src/api/routes.py
@@ -449,7 +449,7 @@ def signup():
     data = request.json
     email = data.get('email')
     password = data.get('password')
-    return;
+    return jsonify({"message": "Step 1"}), 200
 
     if not email or not password:
         return jsonify({"message": "Email and password are required"}), 400
